6d9f37421e78b2bd25ef5e3a0692bcd2f12d839d
Libby Thomas <thomaslibby05@gmail.com>
2020-05-15 15:59:35 -0500

    Initial Commit

---
+++ GoodMythicalApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  GoodMythicalApp
+//
+//  Created by Elizabeth Thomas on 5/15/20.
+//  Copyright © 2020 Libby Thomas. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
